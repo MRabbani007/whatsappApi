f6d65074d9d50943d9e3ec811ce1b00e768cf2c1
Mohamad Rabbani <143613695+MRabbani007@users.noreply.github.com>
2025-02-12 19:13:29 +0500

    Remove strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import React from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/whatsappApi">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/whatsappApi">
+    <App />
+  </BrowserRouter>
 );
